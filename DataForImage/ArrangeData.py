5ab29332d131b7dfedf679ff400408475112b61b
Mitchell <mrjacobs55@gmail.com>
2019-03-17 20:46:58 -0400

    Data by team and season broken down by game
    Signed-off-by: Mitchell <mrjacobs55@gmail.com>

--- DataForImage/ArrangeData.py
+++ DataForImage/ArrangeData.py
@@ -13,7 +13,13 @@ with open('RegularSeasonDetailedResults.csv') as file:
         if not first:
             season.append(row)
         else:
-            print(row)
+            game = [row[1], row[3], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14],
+                    row[15],
+                    row[16], row[17], row[18], row[19], row[20], "-1 loss, 1 win"]
+            print(game)
+            game = [row[1], row[3], row[6], row[7], row[21], row[22], row[23], row[24], row[25], row[26], row[27],
+                    row[28], row[29], row[30], row[31], row[32], row[33], "-1 loss, 1 win"]
+            print(game)
             first = False
 
 with open('NCAATourneyDetailedResults.csv') as file:
@@ -24,9 +30,26 @@ with open('NCAATourneyDetailedResults.csv') as file:
         if not first:
             season.append(row)
         else:
-            print(row)
+            game = [row[1], row[3], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14],
+                    row[15],
+                    row[16], row[17], row[18], row[19], row[20], "-1 loss, 1 win"]
+            print(game)
+            game = [row[1], row[3], row[6], row[7], row[21], row[22], row[23], row[24], row[25], row[26], row[27],
+                    row[28], row[29], row[30], row[31], row[32], row[33], "-1 loss, 1 win"]
+            print(game)
+            print("\n\n")
+            game = [row[1], row[3][1:], row[6][1:], row[7], row[21][1:], row[22][1:], row[23][1:], row[24][1:],
+                    row[25][1:], row[26][1:], row[27][1:], row[28][1:], row[29][1:], row[30][1:], row[31][1:],
+                    row[32][1:], row[33][1:], "-1 loss, 1 win"]
+            print(game)
+        with open('header.csv', 'w') as headerFile:
+            write = csv.writer(headerFile)
+            write.writerow(game)
+
             first = False
 
+
+''' Only Uncomment if you want to edit/create the data files
 for row in season:
     with open("Season+TeamID/" + row[0] + "+" + row[2] + ".csv", 'a') as winningTeam:
         write = csv.writer(winningTeam)
@@ -46,3 +69,4 @@ for row in season:
         lossGame = [row[1], row[3], location, row[7], row[21], row[22], row[23], row[24], row[25], row[26], row[27],
                     row[28], row[29], row[30], row[31], row[32], row[33], -1]
         write.writerow(lossGame)
+'''
